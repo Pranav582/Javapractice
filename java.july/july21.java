1b2aa982596c8516dfdbd7a3a3cfb94c7fb2e183
Pranav Sagne <pranavsagne37@gmail.com>
2026-07-22 12:24:17 +0530

    Add files via upload

---
+++ java.july/july21.java
@@ -0,0 +1,38 @@
+public class july21 {
+    public static void main(String[] args) {
+    //  System.out.println("hello");  
+     
+    //  short rollno = 37;
+
+    // String name = "Pranav";
+    // int pincode = 441302;
+    // //char gender = 'm';
+    // float percentage = 80.39f;
+    // double fees = 38000.57;
+    // boolean isAdult = true;
+    // System.out.println(  rollno + " "+ name + " " + pincode + " " + gender + " " + percentage   + " " + fees + " " + isAdult);
+//22july
+    // String petname = "dollar";
+    // int age = 4;
+    // String breed = "german shepard";
+    // char gender = 'm';
+    // boolean vaccinated = true;
+     
+    // System.out.println( pet + " " + age + " " + breed + " " + gender + " " + vaccinated );
+
+    //fav place
+
+    // String favplace =  "kashmir";
+    // String transportation = "by air";
+    // float cost = 5.6f;
+    // short date =  15;
+    // String month = "july";
+
+    // System.out.println( "place : " + favplace + " ," +  "mode of transport : " + " ," + transportation + 
+    // "estimated cost  :  " + " " + cost + " ," + "date : "+  date +  "month : " + " ," + month );
+
+
+
+
+    }
+}
